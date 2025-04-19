3ff81cbf95084cb47aa9beaef96cb7d55468291d
Asish <doodleaks@Asishs-MacBook-Air.local>
2025-04-19 19:29:07 +1000

    removed some of the horizon ui details from the sign-in

--- src/views/auth/signIn/index.jsx
+++ src/views/auth/signIn/index.jsx
@@ -29,6 +29,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Text,
   useColorModeValue,
 } from "@chakra-ui/react";
 import DefaultAuth from "layouts/auth/Default";
@@ -103,6 +104,14 @@ function SignIn() {
           <Heading color={textColor} fontSize="36px" mb="10px">
             Welcome to Qubit
           </Heading>
+          <Text
+            mb='36px'
+            ms='4px'
+            color={textColor}
+            fontWeight='400'
+            fontSize='md'>
+            Sign in with your Google account to continue
+          </Text>
         </Box>
         <Flex
           zIndex='2'
@@ -110,7 +119,7 @@ function SignIn() {
           w={{ base: "100%", md: "420px" }}
           maxW='100%'
           background='transparent'
-          borderRadius='15px'
+          borderRadius='0px'
           mx={{ base: "auto", lg: "unset" }}
           me='auto'
           mb={{ base: "20px", md: "auto" }}>
